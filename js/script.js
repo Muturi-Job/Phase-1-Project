c03220d2c6d9a78a250c5e146bd5fa9957259b85
Job Muturi <muirimuturi17@gmail.com>
2023-04-03 23:04:26 +0300

    Added functionality to display all players

--- js/script.js
+++ js/script.js
@@ -1,27 +1,91 @@
 //Loads the HTML before running the javascript
 document.addEventListener('DOMContentLoaded', () => {
-    
-
 })
 //Gets the HTML elements
-const teamsSection = document.getElementById('teams')
-const displayAllTeamsButton = document.getElementById('Display-all-teams');
-const chooseSpecificTeamButton = document.getElementById('Choose-specific-team');
-const teamInput = document.getElementById('team-input').value
-const teamId = teamInput.value;
+    const teamsSection = document.getElementById('teams');
+    const playersSection = document.getElementById('players')
+    const displayAllTeamsButton = document.getElementById('Display-all-teams');
+    const chooseSpecificTeamButton = document.getElementById('Choose-specific-team');
+    const submitBtn = document.getElementById('submit-button')
+    const displayAllPlayersButton = document.getElementById('Display-all-players');
+    const chooseSpecificPlayerButton =document.getElementById('Choose-specific-player');
+    const playerInputDiv = document.getElementById('player-input-div');
+    const playerSubmitBtn = document.getElementById('player-submit-button');
+
+
 //Fetches the data of all the teams from the API server then displays it
-function fetchAllTeams () {
-    fetch('https://www.balldontlie.io/api/v1/teams')
-        .then(res => {
-           return res.json()
-        })
-        .then(data => 
-        displayAllTeams(data)
-             )
+    function fetchAllTeams() {
+        fetch('https://www.balldontlie.io/api/v1/teams')
+            .then(res => {
+                return res.json()
+            })
+            .then(data =>
+                displayAllTeams(data)
+            )
     }
-        
-function displayAllTeams(data) {
-    data.data.forEach((data) => {
+
+    function displayAllTeams (data) {
+        data.data.forEach((data) => {
+           
+            const dataContainer = document.createElement('div')
+            const fullName = document.createElement('h4')
+            fullName.textContent = data.full_name;
+            dataContainer.appendChild(fullName);
+            const teamCity = document.createElement('p');
+            teamCity.textContent = `City: ${data.city}`;
+            dataContainer.appendChild(teamCity);
+            const teamConference = document.createElement('p');
+            teamConference.textContent = `Conference; ${data.conference}`;
+            dataContainer.appendChild(teamConference);
+            const teamDivision = document.createElement('p');
+            teamDivision.textContent = `Division: ${data.division}`;
+            dataContainer.appendChild(teamDivision);
+            teamsSection.appendChild(dataContainer);
+            //Upvote and Downvote Button
+    const upvoteButton = document.createElement('button');
+    upvoteButton.textContent = '👍';
+    dataContainer.appendChild(upvoteButton);
+    let upvotes = 0;
+    const upvoteCount = document.createElement('span');
+    upvoteCount.textContent = upvotes;
+    dataContainer.appendChild(upvoteCount);
+    upvoteButton.addEventListener('click', () => {
+        upvotes++;
+        upvoteCount.textContent = upvotes;
+    });
+
+    const downvoteButton = document.createElement('button');
+    downvoteButton.textContent = '👎';
+    dataContainer.appendChild(downvoteButton);
+    let downvotes = 0;
+    const downvoteCount = document.createElement('span');
+    downvoteCount.textContent = downvotes;
+    dataContainer.appendChild(downvoteCount);
+    downvoteButton.addEventListener('click', () => {
+        downvotes++;
+        downvoteCount.textContent = downvotes;
+    });
+
+
+        });
+    }
+    displayAllTeamsButton.addEventListener('click', fetchAllTeams)
+//Fetches the data for a specific team using its ID
+    chooseSpecificTeamButton.addEventListener('click', () => {
+        inputDiv.style.display = "block";
+    });
+    submitBtn.addEventListener("click", () => {
+        event.preventDefault();
+        const teamInput = document.getElementById('team-input')
+        const teamId = teamInput.value;
+        fetch(`https://www.balldontlie.io/api/v1/teams/${teamId}`)
+            .then(res => res.json())
+            .then(data => {
+                displaySpecificTeam(data);
+            })
+            .catch(error => console.log(error));
+      })
+    function displaySpecificTeam (data) {
         const dataContainer = document.createElement('div')
         const fullName = document.createElement('h4')
         fullName.textContent = data.full_name;
@@ -30,30 +94,149 @@ function displayAllTeams(data) {
         teamCity.textContent = `City: ${data.city}`;
         dataContainer.appendChild(teamCity);
         const teamConference = document.createElement('p');
-        teamConference.textContent=`Conference; ${data.conference}`;
+        teamConference.textContent = `Conference; ${data.conference}`;
         dataContainer.appendChild(teamConference);
         const teamDivision = document.createElement('p');
         teamDivision.textContent = `Division: ${data.division}`;
         dataContainer.appendChild(teamDivision);
-        teamsSection.appendChild(dataContainer)
+        teamsSection.appendChild(dataContainer);
+        //Upvote and Downvote Button
+    const upvoteButton = document.createElement('button');
+    upvoteButton.textContent = '👍';
+    dataContainer.appendChild(upvoteButton);
+    let upvotes = 0;
+    const upvoteCount = document.createElement('span');
+    upvoteCount.textContent = upvotes;
+    dataContainer.appendChild(upvoteCount);
+    upvoteButton.addEventListener('click', () => {
+        upvotes++;
+        upvoteCount.textContent = upvotes;
     });
-}
-displayAllTeamsButton.addEventListener('click', fetchAllTeams)
 
-//Fetches the data for a specific team using its ID
+    const downvoteButton = document.createElement('button');
+    downvoteButton.textContent = '👎';
+    dataContainer.appendChild(downvoteButton);
+    let downvotes = 0;
+    const downvoteCount = document.createElement('span');
+    downvoteCount.textContent = downvotes;
+    dataContainer.appendChild(downvoteCount);
+    downvoteButton.addEventListener('click', () => {
+        downvotes++;
+        downvoteCount.textContent = downvotes;
+    });
 
-chooseSpecificTeamButton.addEventListener('click',() => {
-    inputDiv.style.display = "block";
-});
-submitBtn.addEventListener("click", () => {
-    
-    fetch(`https://www.balldontlie.io/api/v1/teams/${teamInput}`)
-        .then(res => res.json())
-        .then(data => {
-        console.log(data)
-        displayAllTeams(data);
+      }
+//Fetches the data  for all the players
+    function fetchAllPlayers () {
+        fetch('https://www.balldontlie.io/api/v1/players')
+            .then(res => res.json())
+            .then(data => {
+                console.log( data)
+                displayAllplayers (data)
             })
-           .catch(error => console.log(error));
+    }
+    function displayAllplayers (data) {
+        data.data.forEach((data) => {
+            const dataContainer = document.createElement('div')
+            const fullName = document.createElement('h4')
+            fullName.textContent = data.first_name + data.last_name;
+            dataContainer.appendChild(fullName);
+            const playerPosition = document.createElement('p');
+            dataContainer.appendChild(playerPosition);
+            const currentTeam = document.createElement('p');
+            currentTeam.textContent = `Current Team: ${data.team.full_name}`;
+            dataContainer.appendChild(currentTeam);
+            const playerHeight = document.createElement('p');
+            dataContainer.appendChild(playerHeight);
+            playersSection.appendChild(dataContainer);
+            //Upvote and Downvote Button
+        const upvoteButton = document.createElement('button');
+        upvoteButton.textContent = '👍';
+        dataContainer.appendChild(upvoteButton);
+        let upvotes = 0;
+        const upvoteCount = document.createElement('span');
+        upvoteCount.textContent = upvotes;
+        dataContainer.appendChild(upvoteCount);
+        upvoteButton.addEventListener('click', () => {
+            upvotes++;
+            upvoteCount.textContent = upvotes;
+    });
+
+        const downvoteButton = document.createElement('button');
+        downvoteButton.textContent = '👎';
+        dataContainer.appendChild(downvoteButton);
+        let downvotes = 0;
+        const downvoteCount = document.createElement('span');
+        downvoteCount.textContent = downvotes;
+        dataContainer.appendChild(downvoteCount);
+        downvoteButton.addEventListener('click', () => {
+            downvotes++;
+            downvoteCount.textContent = downvotes;
+        });
+
+        });
+    }
+    displayAllPlayersButton.addEventListener('click', fetchAllPlayers)
+//Fetches data for a specific player
+    chooseSpecificPlayerButton.addEventListener('click', () => {
+        playerInputDiv.style.display = "block";
+    });
+    playerSubmitBtn.addEventListener("click", () => {
+        event.preventDefault();
+        const playerInput = document.getElementById('player-input')
+        const playerName = playerInput.value;
+   
+        fetch(`https://www.balldontlie.io/api/v1/players?search=${playerName}`)
+            .then (res => res.json())
+            .then ((data) => {
+                displaySpecificPlayer(data)
+            })
+    
     })
+    function displaySpecificPlayer(data) {
+        data.data.forEach((data) => {
+        const dataContainer = document.createElement('div')
+            const fullName = document.createElement('h4')
+            fullName.textContent = `${data.first_name}  ${data.last_name}`;
+            dataContainer.appendChild(fullName);
+            const currentTeam = document.createElement('p');
+            currentTeam.textContent = `Current Team: ${data.team.full_name}`;
+            dataContainer.appendChild(currentTeam);
+            playersSection.appendChild(dataContainer);
+            //Upvote and Downvote Button
+    const upvoteButton = document.createElement('button');
+    upvoteButton.textContent = '👍';
+    dataContainer.appendChild(upvoteButton);
+    let upvotes = 0;
+    const upvoteCount = document.createElement('span');
+    upvoteCount.textContent = upvotes;
+    dataContainer.appendChild(upvoteCount);
+    upvoteButton.addEventListener('click', () => {
+        upvotes++;
+        upvoteCount.textContent = upvotes;
+    });
+
+    const downvoteButton = document.createElement('button');
+    downvoteButton.textContent = '👎';
+    dataContainer.appendChild(downvoteButton);
+    let downvotes = 0;
+    const downvoteCount = document.createElement('span');
+    downvoteCount.textContent = downvotes;
+    dataContainer.appendChild(downvoteCount);
+    downvoteButton.addEventListener('click', () => {
+        downvotes++;
+        downvoteCount.textContent = downvotes;
+    });
+
+        })
+        
+    }
+
+
+
+
+
+    
+
 
 
